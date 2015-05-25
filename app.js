4891ede9e34dfbcf7b734315d0195126add29777
zimmed <dzimmelman@gmail.com>
2015-05-24 22:05:17 -0400

    Trying to get student router working

--- app.js
+++ app.js
@@ -21,6 +21,68 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* STUDENT HOMEPAGES */
+
+function load_index (path) {
+    if (path && path.charAt(path.length - 1) !== '/') path += '/';
+    if (fs.existsSync(path + 'index.html')) {
+        fs.sendFile(path + 'index.html');
+    } else if (fs.existsSync(path + 'index.htm')) {
+        fs.sendFile(path + 'index.htm');
+    } else if (fs.existsSync(path + 'default.html')) {
+        fs.sendFile(path + 'default.html');
+    } else if (fs.existsSync(path + 'default.htm')) {
+        fs.sendFile(path + 'default.htm');
+    } else {
+        var error = new Error('Not Found');
+        error.status = 404;
+        res.render('error', {
+            error: error,
+            message: "Could not locate index page."
+        });
+    }
+}
+
+app.get(/^(\/\~|\~)([a-zA-Z0-9])(\/$|$|\/(.+)$)/, function (req, res) {
+    var user = req.params[2].toLowerCase(),
+        path = req.params[4],
+        base = '/home/students/' + user + '/public_html';
+    fs.exists(base, function (exists) {
+        if (!exists) {
+            var error = new Error('Not Found');
+            error.status = 404;
+            res.render('error', {
+                message: "User not found: " + user,
+                error: error
+            });
+        } else {
+            if (path) path = path.replace(/\.\.\//g, '');
+            if (path && path.length > 0) {
+                fs.stats(base + '/' + path, function (err, stats) {
+                    if (stats && stats.isFile()) {
+                        res.sendFile(base + '/' + path);
+                    } else if (stats && stats.isDirectory()) {
+                        load_index(base + '/' + path, res);
+                    } else {
+                        if (err) {
+                            console.log('Fs.stats err >>>');
+                            console.log(err);
+                        }
+                        var error = new Error('Not Found');
+                        error.status = 404;
+                        res.render('error', {
+                            message: "File not found: /" + path,
+                            error: error
+                        });
+                    }
+                });
+            } else {
+                load_index(base);
+            }
+        }
+    });
+});
+
 app.use('/', index);
 
 // catch 404 and forward to error handler
